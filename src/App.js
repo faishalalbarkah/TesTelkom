c9d58cfde2405ce4a4810c3e86e1173f85763657
faishalalbarkah <54970019+faishalalbarkah@users.noreply.github.com>
2020-04-09 13:31:30 +0700

    hampir jadi

--- src/App.js
+++ src/App.js
@@ -1,29 +1,100 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import { Form, Card, Image, Icon } from "semantic-ui-react";
 import "./App.css";
-import Axios from "axios";
-import FormUser from "./components/FormUser";
 
-class App extends Component {
-  getUser = (e) => {
-    e.preventDefault();
-    const user = e.target.elements.username.value;
-    // console.log(user);
-    Axios.get(`https://api.github.com/users/${user}`).then((res) => {
-      console.log(res);
-    });
+function App() {
+  const [name, setName] = useState("");
+  const [userName, setUsername] = useState("");
+  const [followers, setFollowers] = useState("");
+  const [following, setFollowing] = useState("");
+  const [repos, setRepos] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [userInput, setUserInput] = useState("");
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/example`)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
+  const setData = ({
+    name,
+    login,
+    followers,
+    following,
+    public_repos,
+    avatar_url,
+  }) => {
+    setName(name);
+    setUsername(login);
+    setFollowers(followers);
+    setFollowing(following);
+    setRepos(public_repos);
+    setAvatar(avatar_url);
   };
 
-  //https://api.github.com/users/faishalalbarkah
-  render() {
-    return (
-      <div className="App">
-        <header style={{ height: 150, backgroundColor: "red", marginTop: -40 }}>
-          <p style={{ fontSize: 45, fontWeight: "bold" }}>Call Github</p>
-        </header>
-        <FormUser getUser={this.getUser} />
+  const handleSearch = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    fetch(`https://api.github.com/users/${userInput}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message) {
+          setError(data.message);
+        } else {
+          setData(data);
+        }
+      });
+  };
+  return (
+    <div>
+      <div className="navbar">Get API Github</div>
+      <div className="search">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Input
+              placeholder="Github User"
+              name="Github user"
+              onChange={handleSearch}
+            />
+            <Form.Button content="Search" />
+          </Form.Group>
+        </Form>
       </div>
-    );
-  }
+      <div className="card">
+        <Card>
+          <Image src={avatar} wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>{name}</Card.Header>
+            <Card.Header>{userName}</Card.Header>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name="user" />
+              {followers} Followers
+            </a>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name="user" />
+              {repos} Repository
+            </a>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name="user" />
+              {following} Following
+            </a>
+          </Card.Content>
+        </Card>
+      </div>
+    </div>
+  );
 }
+
 export default App;
